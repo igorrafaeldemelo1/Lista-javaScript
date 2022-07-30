b2540f0b590c63deba46e0759678209232c78ec5
igorrafaeldemelo1 <86748756+igorrafaeldemelo1@users.noreply.github.com>
2022-07-30 11:22:19 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let nomes = [
+	"jamilton",
+	"josé",
+	"letícia",
+	"maria",
+	"carlos",
+	"pedro",
+	"marcela",
+	"carla",
+	"carlos"
+]
+
+function pesquisarNome(){
+
+	let nomePesquisa = document.getElementById('campoNome').value
+	let intensLista = ''
+
+	for(indice in nomes){
+		let nome = nomes[indice]
+		if( nomePesquisa == nome ){
+			intensLista += `
+			<li class="list-group-item">
+				${nome}
+			</li>`
+		}
+	}
+
+	document.getElementById('lista').innerHTML = intensLista
+
+}
+
+function carregarNomes(){
+
+	let intensLista = ''
+	for(indice in nomes){
+		
+		let nome = nomes[indice]
+
+		intensLista += `
+			<li class="list-group-item">
+				${nome}
+			</li>`
+
+		//alert(intensLista)
+	}
+
+	document.getElementById('lista').innerHTML = intensLista
+
+}
